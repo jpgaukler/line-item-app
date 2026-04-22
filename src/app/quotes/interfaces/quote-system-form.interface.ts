7d6df752ac1967cb3d5ffcb1b1f7107176c455c8
Justin Gaukler <jpgaukler@gmail.com>
2026-04-21 20:47:05 -0400

    Deleted unused interfaces.

--- src/app/quotes/interfaces/quote-system-form.interface.ts
+++
@@ -1,8 +0,0 @@
-import { FormArray, FormControl, FormGroup } from '@angular/forms';
-import { QuoteItemForm } from './quote-item-form.interface';
-
-export interface QuoteSystemForm {
-  name: FormControl<string>;
-  price: FormControl<number>;
-  items: FormArray<FormGroup<QuoteItemForm>>;
-}
