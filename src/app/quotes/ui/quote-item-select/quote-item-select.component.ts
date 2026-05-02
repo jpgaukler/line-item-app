5c4962bd7ffc158d0f5fa22060cc0c0db38481d8
Justin Gaukler <jpgaukler@gmail.com>
2026-05-02 11:22:07 -0400

    Add logic for custom display text and value.

--- src/app/quotes/ui/quote-item-select/quote-item-select.component.ts
+++ src/app/quotes/ui/quote-item-select/quote-item-select.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component, ElementRef, inject, input, model, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { ProductSelectionOption } from '../../../products/interfaces/product-selection-option.interface';
 import { ProductSelection } from '../../../products/interfaces/product-selection.interface';
 import { QuoteItemSelection } from '../../interfaces/quote-item-selection.interface';
 
 @Component({
   selector: 'app-quote-item-select',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './quote-item-select.component.html',
   host: {
     '(document:click)': 'onDocumentClick($event)',
@@ -18,7 +19,6 @@ export class QuoteItemSelectComponent {
   productSelection = input.required<ProductSelection>();
   quoteItemSelection = model.required<QuoteItemSelection>();
   isOpen = signal<boolean>(false);
-  isEditingCustomText = signal<boolean>(false);
 
   optionClick(option: ProductSelectionOption) {
     this.quoteItemSelection.update((selection) => ({
@@ -30,7 +30,41 @@ export class QuoteItemSelectComponent {
     this.isOpen.set(false);
   }
 
-  onDocumentClick(event: MouseEvent) {
+  toggleCustomValue(): void {
+    this.quoteItemSelection.update((selection) => {
+      const isCustom = !selection.isCustomValue;
+      const matchingOption = this.productSelection().options.find(
+        (i) => i.value === selection.value,
+      );
+
+      return {
+        ...selection,
+        isCustomValue: isCustom,
+        displayText: isCustom
+          ? selection.displayText
+          : (matchingOption?.displayText ?? this.productSelection().options[0].displayText),
+        value: isCustom
+          ? selection.value
+          : (matchingOption?.value ?? this.productSelection().options[0].value),
+      };
+    });
+  }
+
+  displayTextChange(displayText: string): void {
+    this.quoteItemSelection.update((selection) => ({
+      ...selection,
+      displayText: displayText,
+    }));
+  }
+
+  valueChange(value: string): void {
+    this.quoteItemSelection.update((selection) => ({
+      ...selection,
+      value: value,
+    }));
+  }
+
+  onDocumentClick(event: MouseEvent): void {
     const clickedInside = this.el.nativeElement.contains(event.target);
 
     // Close if the click was outside
