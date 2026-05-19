6abf1e62e67fcf02733a4644ead8b7303313c97a
Justin Gaukler <jpgaukler@gmail.com>
2026-05-18 21:31:49 -0400

    Add auth0 package. Add environments files.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,8 @@ import {
   provideZonelessChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAuth0 } from '@auth0/auth0-angular';
+import { environment } from '../environments/environment.development';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
@@ -13,5 +15,12 @@ export const appConfig: ApplicationConfig = {
     provideZonelessChangeDetection(),
     provideRouter(routes),
     provideHttpClient(),
+    provideAuth0({
+      domain: environment.auth0.domain,
+      clientId: environment.auth0.clientId,
+      authorizationParams: {
+        redirect_uri: window.location.origin,
+      },
+    }),
   ],
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  auth0: {
+    domain: 'dev-q327it81x3el6nc1.us.auth0.com',
+    clientId: 'nn6J31sncLNLzF5drsh2GSKjnRHynwrT',
+  },
+};
