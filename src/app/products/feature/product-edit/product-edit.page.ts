89973dee14c5e50f9bfe318bbaf747f9d549b775
Justin Gaukler <jpgaukler@gmail.com>
2026-05-22 21:57:07 -0400

    Remove reactive forms from product edit page and use signal forms instead.

--- src/app/products/feature/product-edit/product-edit.page.ts
+++ src/app/products/feature/product-edit/product-edit.page.ts
@@ -1,9 +1,9 @@
 import { CdkCopyToClipboard } from '@angular/cdk/clipboard';
-import { CdkDrag, CdkDragDrop, CdkDropList, moveItemInArray } from '@angular/cdk/drag-drop';
+import { CdkDrag, CdkDropList } from '@angular/cdk/drag-drop';
 import { CommonModule } from '@angular/common';
 import { Component, effect, inject, OnDestroy, signal } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormField } from '@angular/forms/signals';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { map } from 'rxjs';
 import { LayoutService } from '../../../layout/data-access/layout.service';
@@ -11,73 +11,37 @@ import { ButtonDirective } from '../../../shared/ui/button-primary.directive';
 import { logJsonSize } from '../../../shared/utils/data-utils';
 import { ProductEditService } from '../../data-access/product-edit.service';
 import { ProductFormService } from '../../data-access/product-form.service';
-import { ProductForm } from '../../interfaces/product-form.interface';
-import { ProductInputOptionForm } from '../../interfaces/product-input-option-form.interface';
-import {
-  MAX_PRODUCT_DESCRIPTION_LENGTH,
-  MAX_PRODUCT_NAME_LENGTH,
-  Product,
-} from '../../interfaces/product.interface';
+import { ProductInputComponent } from '../../ui/product-input/product-input.component';
 
 @Component({
   selector: 'app-products',
   imports: [
     CommonModule,
-    FormsModule,
     ButtonDirective,
-    ReactiveFormsModule,
     CdkDropList,
     CdkDrag,
     CdkCopyToClipboard,
     RouterLink,
+    FormField,
+    ProductInputComponent,
   ],
   templateUrl: './product-edit.page.html',
   providers: [ProductEditService, ProductFormService],
 })
 export class ProductEditPage implements OnDestroy {
   private readonly layoutService = inject(LayoutService);
-  private readonly productFormService = inject(ProductFormService);
   public readonly productEditService = inject(ProductEditService);
   private readonly activatedRoute = inject(ActivatedRoute);
 
   showDebug = signal<boolean>(false);
 
-  maxNameLength = MAX_PRODUCT_NAME_LENGTH;
-  maxDescriptionLength = MAX_PRODUCT_DESCRIPTION_LENGTH;
   productId = toSignal(this.activatedRoute.paramMap.pipe(map((params) => params.get('productId'))));
-
-  productForm: FormGroup<ProductForm> = this.productFormService.toProductForm({
-    id: '',
-    name: '',
-    description: '',
-    productCodeFormula: '',
-    inputs: [],
-    adders: [],
-  });
-
-  get inputsArray() {
-    return this.productForm.controls.inputs;
-  }
-
-  optionsAt(inputIndex: number) {
-    return this.productForm.controls.inputs.at(inputIndex).controls.options;
-  }
-
-  defaultOptionAt(inputIndex: number) {
-    return this.productForm.controls.inputs.at(inputIndex).controls.defaultOptionIndex;
-  }
 
   constructor() {
     this.layoutService.updateBreadcrumbs$.next([
       { label: 'Home', url: '/' },
       { label: 'Products', url: '/products' },
     ]);
-
-    effect(() => {
-      if (this.productEditService.loaded()) {
-        this.initializeProductForm(this.productEditService.product());
-      }
-    });
 
     effect(() => {
       const productId = this.productId();
@@ -92,115 +56,46 @@ export class ProductEditPage implements OnDestroy {
     this.layoutService.clearBreadcrumbs$.next();
   }
 
-  initializeProductForm(product: Product): void {
-    this.productForm = this.productFormService.toProductForm(product);
+  addInput() {
+    this.productEditService.productForm.product.inputs().value.update((inputs) => ({
+      ...inputs,
+      options: [
+        ...inputs,
+        {
+          name: `Input (${inputs.length + 1})`,
+          defaultOptionIndex: 0,
+          allowCustomValue: false,
+          options: [{ displayText: 'Option (1)', value: 'Value (1)' }],
+        },
+      ],
+    }));
   }
 
-  addInput(): void {
-    this.inputsArray.push(
-      this.productFormService.toProductInputForm({
-        name: `Input (${this.inputsArray.length + 1})`,
-        defaultOptionIndex: 0,
-        allowCustomValue: false,
-        options: [{ displayText: 'Option (1)', value: 'Value (1)' }],
-      }),
-    );
+  removeInput(index: number) {
+    const inputsArray = this.productEditService.productForm.product.inputs().value();
 
-    this.productForm.markAsDirty();
-  }
-
-  removeInput(inputIndex: number): void {
-    this.inputsArray.removeAt(inputIndex);
-    this.productForm.markAsDirty();
-  }
-
-  addOption(inputIndex: number): void {
-    const optionsArray = this.optionsAt(inputIndex);
-
-    optionsArray.push(
-      this.productFormService.toProductInputOptionForm({
-        displayText: `Option (${optionsArray.length + 1})`,
-        value: `Value (${optionsArray.length + 1})`,
-      }),
-    );
-
-    this.productForm.markAsDirty();
-  }
-
-  removeOption(inputIndex: number, optionIndex: number): void {
-    const optionsArray = this.optionsAt(inputIndex);
-
-    if (optionsArray.length <= 1) {
+    if (inputsArray.length <= 1) {
       return;
     }
 
-    optionsArray.removeAt(optionIndex);
-
-    const defaultOptionControl = this.defaultOptionAt(inputIndex);
-    const currentDefaultIndex = defaultOptionControl.value;
-
-    if (currentDefaultIndex === optionIndex) {
-      defaultOptionControl.setValue(currentDefaultIndex - 1);
-    }
-
-    this.productForm.markAsDirty();
-  }
-
-  setDefaultOption(inputIndex: number, optionIndex: number): void {
-    const defaultOptionControl = this.defaultOptionAt(inputIndex);
-    defaultOptionControl.setValue(optionIndex);
-    this.productForm.markAsDirty();
-  }
-
-  reorderOption(inputIndex: number, event: CdkDragDrop<FormGroup<ProductInputOptionForm>>): void {
-    const previousIndex = event.previousIndex;
-    const currentIndex = event.currentIndex;
-
-    if (previousIndex === currentIndex) {
-      return;
-    }
-
-    const optionsArray = this.optionsAt(inputIndex);
-    const defaultOptionControl = this.defaultOptionAt(inputIndex);
-
-    // Move the options FormGroup
-    moveItemInArray(optionsArray.controls, previousIndex, currentIndex);
-    optionsArray.updateValueAndValidity(); // required because Angular doesn't detect this mutation automatically
-
-    // Fix the default option index
-    const currentDefaultIndex = defaultOptionControl.value;
-
-    if (currentDefaultIndex === previousIndex) {
-      // The default itself was moved
-      defaultOptionControl.setValue(currentIndex);
-    } else if (previousIndex < currentIndex) {
-      // non-default moved down - items between shift up
-      if (currentDefaultIndex > previousIndex && currentDefaultIndex <= currentIndex) {
-        defaultOptionControl.setValue(currentDefaultIndex - 1);
-      }
-    } else if (previousIndex > currentIndex) {
-      // non-default moved up - items between shift down
-      if (currentDefaultIndex >= currentIndex && currentDefaultIndex < previousIndex) {
-        defaultOptionControl.setValue(currentDefaultIndex + 1);
-      }
-    }
-
-    this.productForm.markAsDirty();
+    // remove option
+    this.productEditService.productForm.product
+      .inputs()
+      .value.update((inputs) => inputs.filter((_, i) => i !== index));
   }
 
   submit(): void {
-    if (this.productForm.invalid) {
-      this.productForm.markAllAsTouched();
-      return;
-    }
-
-    this.productEditService.updateProduct$.next(
-      this.productFormService.toProduct(this.productForm),
-    );
+    // if (this.productForm.invalid) {
+    //   this.productForm.markAllAsTouched();
+    //   return;
+    // }
+    // this.productEditService.updateProduct$.next(
+    //   this.productFormService.toProduct(this.productForm),
+    // );
   }
 
   reset(): void {
-    this.initializeProductForm(this.productEditService.product());
+    // this.initializeProductForm(this.productEditService.product());
   }
 
   calculateJsonSize() {
