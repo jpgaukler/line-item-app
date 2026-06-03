a4564ccb964bd1fadcd7c80520638b22ea5c64bd
Justin Gaukler <jpgaukler@gmail.com>
2026-06-03 00:43:53 -0400

    Added setup for api authentication.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import {
   ApplicationConfig,
   inject,
@@ -6,7 +6,7 @@ import {
   provideEnvironmentInitializer,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { AuthService, provideAuth0 } from '@auth0/auth0-angular';
+import { authHttpInterceptorFn, AuthService, provideAuth0 } from '@auth0/auth0-angular';
 import { environment } from '../environments/environment';
 import { routes } from './app.routes';
 
@@ -19,18 +19,17 @@ export const appConfig: ApplicationConfig = {
       clientId: environment.auth0.clientId,
       authorizationParams: {
         redirect_uri: window.location.origin,
-        // audience: environment.auth0.audience,
+        audience: environment.auth0.apiAudience,
       },
-      // httpInterceptor: {
-      //   allowedList: [`${environment.apiBaseUrl}/api/*`],
-      // },
+      httpInterceptor: {
+        allowedList: [`${environment.apiBaseUrl}/api/*`],
+      },
       useRefreshTokens: true,
       cacheLocation: 'localstorage',
     }),
+    provideHttpClient(withInterceptors([authHttpInterceptorFn])),
     provideEnvironmentInitializer(() => {
       inject(AuthService); // inject AuthService before bootstrap to handle Auth0 redirects
     }),
-    provideHttpClient(),
-    // provideHttpClient(withInterceptors([authHttpInterceptorFn])),
   ],
 };
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,10 +1,10 @@
 import { Environment } from './environment.interface';
 
 export const environment: Environment = {
-  apiBaseUrl: 'http://localhost:3001',
+  apiBaseUrl: 'https://localhost:7165',
   auth0: {
     domain: 'login.dev.line-item.app',
     clientId: 'N8B3KhcpMYABAjSjwZ3jBbqDXE8ZD5gG',
-    audience: 'https://line-item.app',
+    apiAudience: 'https://line-item.app/api',
   },
 };
